821ad9c46a0af6bc6da66c5cc43c6f65c2edf463
alokkumarak <alok.sonaili@gmail.com>
2021-05-28 19:31:25 +0530

    time duration added

--- assets/connection.php
+++ assets/connection.php
@@ -3,11 +3,11 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$db =  "BitsAndPieces";
-$conn = new mysqli($hostname,$username,$password,$db);
-if($conn->connect_error){
-die("connection error".$conn->connect_error);
-header('location: 404.html');
+$db =  "bitsandpieces";
+$conn = new mysqli($hostname, $username, $password, $db);
+if ($conn->connect_error) {
+    die("connection error" . $conn->connect_error);
+    header('location: 404.html');
 }
 session_start();
 error_reporting('1');
